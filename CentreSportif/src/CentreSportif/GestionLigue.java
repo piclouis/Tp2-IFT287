174497b5a3b5e0fab4446c13a24602e173421dd0
laza2001 <antoine.lazure@usherbrooke.ca>
2018-10-19 12:36:38 -0400

    ajout au gestion

--- CentreSportif/src/CentreSportif/GestionLigue.java
+++ CentreSportif/src/CentreSportif/GestionLigue.java
@@ -3,4 +3,53 @@ package CentreSportif;
 import java.sql.*;
 
 public class GestionLigue {
+
+    private TableLigues ligues;
+    private TableEquipes equipes;
+    private TableResultats resultats;
+    private Connexion cx;
+
+    public GestionLigue(TableLigues ligues, TableEquipes equipes, TableResultats resultats) throws IFT287Exception {
+        this.cx = ligues.getConnexion();
+        if (ligues.getConnexion() != equipes.getConnexion())
+            throw new IFT287Exception("Les instances de TableLigues et de TableEquipes n'utilisent pas la même connexion au serveur");
+        if (ligues.getConnexion() != resultats.getConnexion())
+            throw new IFT287Exception("Les instances de TableLigues et de TableResultats n'utilisent pas la même connexion au serveur");
+        this.resultats = resultats;
+        this.equipes = equipes;
+        this.ligues = ligues;
+    }
+
+    public void ajouterLigue(String nomLigue,int nbJoueurMaxParEquipe) throws SQLException, IFT287Exception, Exception {
+        try {
+
+
+        } catch (Exception e) {
+            cx.rollback();
+            throw e;
+        }
+    }
+
+    public void supprimerLigue(String nomLigue) throws SQLException, IFT287Exception, Exception {
+        try {
+
+
+        } catch (Exception e) {
+            cx.rollback();
+            throw e;
+        }
+    }
+
+
+    public void afficherLigue(String nomLigue) throws SQLException, IFT287Exception, Exception {
+        try {
+
+
+        } catch (Exception e) {
+            cx.rollback();
+            throw e;
+        }
+    }
+
+
 }
--- CentreSportif/src/CentreSportif/GestionResultat.java
+++ CentreSportif/src/CentreSportif/GestionResultat.java
@@ -3,18 +3,18 @@ package CentreSportif;
 import java.sql.*;
 
 public class GestionResultat {
-    private TableResultats resultat;
-    private TableEquipes equipe;
+    private TableResultats resultats;
+    private TableEquipes equipes;
     private Connexion cx;
 
     //Creation d'une instance
 
-    public GestionResultat(TableResultats resultat, TableEquipes equipe) throws IFT287Exception {
-        this.cx = resultat.getConnexion();
-        if (resultat.getConnexion() != equipe.getConnexion())
+    public GestionResultat(TableResultats resultats, TableEquipes equipes) throws IFT287Exception {
+        this.cx = resultats.getConnexion();
+        if (resultats.getConnexion() != equipes.getConnexion())
             throw new IFT287Exception("Les instances de resultat et de equipe n'utilisent pas la même connexion au serveur");
-        this.resultat = resultat;
-        this.equipe = equipe;
+        this.resultats = resultats;
+        this.equipes = equipes;
     }
 
     /**
@@ -24,12 +24,20 @@ public class GestionResultat {
     public void ajouterResultat(String nomEquipeA, int scoreEquipeA, String nomEquipeB, int scoreEquipeB)
             throws SQLException, IFT287Exception, Exception {
         try {
+            // Verifie si les equipes existent
+            TupleEquipe tupleEquipeA = getEquipe(nomEquipeA);
+            if(tupleEquipeA == null)
+                throw new IFT287Exception("Nom d'équipe A : " + nomEquipeA + "inexistant");
+            TupleEquipe tupleEquipeB = getEquipe(nomEquipeB);
+            if(tupleEquipeB == null)
+                throw new IFT287Exception("Nom d'équipe B : " + nomEquipeB + "inexistant");
             // Vérifie si le resultat existe déja
-            if (resultat.existe(nomEquipeA, nomEquipeB)) // a voir
+            if (resultats.existe(nomEquipeA, nomEquipeB))
                 throw new IFT287Exception("Équipe A : " + nomEquipeA + " et équipe B : " + nomEquipeB + " déja existant.");
 
+
             // Ajout d'un resultat dans la table des livres
-            resultat.ajouterResultat(nomEquipeA, scoreEquipeA, nomEquipeB, scoreEquipeB);
+            resultats.ajouterResultat(nomEquipeA, scoreEquipeA, nomEquipeB, scoreEquipeB);
 
             // Commit
             cx.commit();
